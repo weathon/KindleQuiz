9c702e87a0163fb99c4398425ea32dde069b9871
weathon <wguo6358@gmail.com>
2020-04-29 22:10:47 -0500

    修改邏輯

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # 还是两个排序，优先掌握程度然后是时间？
 from flask import Flask
 import random
+import queue
 app = Flask(__name__)
 
 f=open("export.txt","r")
@@ -12,15 +13,37 @@ energypoints=int(f.read())
 f.close()
 
 #If passed, add -1 to the end, means random, if not, add it self
+myq=queue.Queue()
 
+notagain={}
+mydict={}
+#If point==2, add to here, but 如果三次抽到一張卡，那麽還是要檢查（模擬過一會檢測）
+
+for i in [-1,-1,-1,-1,-1,-1,-1]:
+    myq.put(i)
 
 
 words_list=words.split("\n\n")
-
+for i in words_list:
+    stmp=i.split("******")
+    notagain[stmp[0]]=0
+    mydict[stmp[0]]=stmp[1]
 
 @app.route('/getword')
 def getword():
-    return words_list[random.randint(0,len(words_list)-1)]
+    stattmp=myq.get()
+    print(stattmp)
+    if stattmp==-1:
+        return words_list[random.randint(0,len(words_list)-1)]
+    #elif notagain[stattmp]==1:#got here once
+    #    return words_list[random.randint(0,len(words_list)-1)]
+    #    notagain[stattmp]=0
+    else:
+        return stattmp+"******"+mydict[stattmp]
+
+@app.route('/queue')
+def returnq():
+    return myq.size()
 
 @app.route('/')
 def main():
@@ -34,6 +57,10 @@ def feedback(points,word):
     global energypoints
     energypoints+=int(points)
     print(points)
+    if points==0:
+        myq.put(word)
+    elif points==2:
+        myq.put(-1) 
     return ""
 
 @app.route("/getpoints")
